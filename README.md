bac28d7155c7410fab2d21c178efc4e4e896e7f1
Shkafenko <klucenkovegor2@gmail.com>
2024-11-15 21:17:51 +0000

    marked up the site

---
+++ README.md
@@ -0,0 +1,15 @@
+CLICKER BY SHKAFENKO
+
+key features:
+
+clickable character and background
+while click on character, income increases
+
+boost button, increases income with respect to multiplier
+
+characters, background store
+income depends on character
+
+instagram link
+
+adaptive interface
--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 "use strict"
 
 let ptsCounter = 0;
+let boostCost = 500;
+let boostMaximizer = 1;
+let clickValue = 1;
 
-document.getElementById('ptsCounter').innerText = ptsCounter;
+document.querySelector('.boost-text').innerText = `BOOST x${boostMaximizer} ${boostCost}`;
+
+document.querySelector('.pts-counter').innerText = ptsCounter;
+
+let clickProcessor = () =>
+{
+    ptsCounter += clickValue*boostMaximizer;
+    document.querySelector('.pts-counter').innerText = ptsCounter;
+}
+
+let characterClickProcessor = (event) =>
+{
+    event.stopPropagation();
+    ptsCounter += clickValue*boostMaximizer*2;
+    document.querySelector('.pts-counter').innerText = ptsCounter;
+}
+
+let boostProcessor = (event) =>
+{
+    event.stopPropagation();
+    if (ptsCounter >= boostCost)
+        {
+        ptsCounter -= boostCost;
+        boostCost *= 2;
+        boostMaximizer += 1;
+        document.querySelector('.pts-counter').innerText = ptsCounter;
+        document.querySelector('.boost-text').innerText = `BOOST x${boostMaximizer} ${boostCost}`;
+    }
+    else
+        alert('not enough points!');
+}
+
+document.querySelector('.character-pic').addEventListener('click', characterClickProcessor);
+
+document.querySelector('.body').addEventListener('click', clickProcessor);
+
+document.querySelector('.booster-button').addEventListener('click', boostProcessor);
