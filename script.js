161b3b870a3a89f737b25e56a673a97b0044db71
Shkafenko <klucenkovegor2@gmail.com>
2024-11-16 15:52:02 +0000

    added adaptive interface

--- script.js
+++ script.js
@@ -1,71 +1,78 @@
 "use strict"
 
-let stewie = {
-    unlocked: 1,
-    picLink: 'pics/stewie-icon.jpg',
-    picClass: '.stewie-pic',
-    nameClass: '.stewie-text',
-    charPic: 'pics/stewie.png',
-    price: 0,
-    clickValue: 2,
-    name: 'Stewie',
-}
-let peter = {
-    unlocked: 0,
-    picLink: 'pics/peter-icon.jpg',
-    picClass: '.peter-pic',
-    nameClass: '.peter-text',
-    charPic: 'pics/peter.png',
-    price: 500,
-    clickValue: 50,
-    name: 'Peter',
-}
+const characters = [
 
-let cleveland = {
-    unlocked: 0,
-    picLink: 'pics/cleveland-icon.jpg',
-    picClass: '.cleveland-pic',
-    nameClass: '.cleveland-text',
-    charPic: 'pics/cleveland.png',
-    price: 6000,
-    clickValue: -50,
-    name: 'Cleveland',
-}
+    {
+        unlocked: 1,
+        picLink: 'pics/stewie-icon.jpg',
+        picClass: '.stewie-pic',
+        nameClass: '.stewie-text',
+        charPic: 'pics/stewie.png',
+        price: 0,
+        clickValue: 2,
+        name: 'Stewie',
+    },
+    {
+        unlocked: 0,
+        picLink: 'pics/peter-icon.jpg',
+        picClass: '.peter-pic',
+        nameClass: '.peter-text',
+        charPic: 'pics/peter.png',
+        price: 500,
+        clickValue: 50,
+        name: 'Peter',
+    },
 
-let wp_default = {
-    unlocked: 1,
-    price: 0,
-    wpLink: 'url(pics/default.jpg)',
-    picLink: 'pics/default-icon.jpg',
-    picClass: '.wp-default-pic',
-    nameClass: '.wp-default-text',
-    name: 'Default',
-}
+    {
+        unlocked: 0,
+        picLink: 'pics/cleveland-icon.jpg',
+        picClass: '.cleveland-pic',
+        nameClass: '.cleveland-text',
+        charPic: 'pics/cleveland.png',
+        price: 6000,
+        clickValue: -50,
+        name: 'Cleveland',
+    },
 
-let wp_krichev = {
-    unlocked: 0,
-    price: 1000,
-    wpLink: 'url(pics/krichev.jpg)',
-    picLink: 'pics/krichev-icon.jpg',
-    picClass: '.wp-krichev-pic',
-    nameClass: '.wp-krichev-text',
-    name: 'Krichev',
-}
+];
 
-let wp_tehran = {
-    unlocked: 0,
-    price: 10000,
-    wpLink: 'url(pics/tehran.webp)',
-    picLink: 'pics/tehran-icon.jpg',
-    picClass: '.wp-tehran-pic',
-    nameClass: '.wp-tehran-text',
-    name: 'Tehran',
-}
+const wallpapers = [
+
+    {
+        unlocked: 1,
+        price: 0,
+        wpLink: 'url(pics/default.jpg)',
+        picLink: 'pics/default-icon.jpg',
+        picClass: '.wp-default-pic',
+        nameClass: '.wp-default-text',
+        name: 'Default',
+    },
+
+    {
+        unlocked: 0,
+        price: 1000,
+        wpLink: 'url(pics/krichev.jpg)',
+        picLink: 'pics/krichev-icon.jpg',
+        picClass: '.wp-krichev-pic',
+        nameClass: '.wp-krichev-text',
+        name: 'Krichev',
+    },
+
+    {
+        unlocked: 0,
+        price: 10000,
+        wpLink: 'url(pics/tehran.webp)',
+        picLink: 'pics/tehran-icon.jpg',
+        picClass: '.wp-tehran-pic',
+        nameClass: '.wp-tehran-text',
+        name: 'Tehran',
+    },
+]
 
 let ptsCounter = 0;
 let boostCost = 500;
 let boostMaximizer = 1;
-let activeCharacter = stewie;
+let activeCharacter = characters[0];
 
 document.body.style.backgroundImage = "url(pics/default.jpg)";
 
@@ -158,14 +165,14 @@ document.querySelector('.booster-button').addEventListener('click', boostProcess
 
 document.querySelector('.insta-pic').addEventListener('click', linkProcessor);
 
-document.querySelector('.stewie-pic').addEventListener('click', function() { characterBuyProcessor(event, stewie); });
+document.querySelector('.stewie-pic').addEventListener('click', function() { characterBuyProcessor(event, characters[0]); });
 
-document.querySelector('.peter-pic').addEventListener('click', function() { characterBuyProcessor(event, peter); });
+document.querySelector('.peter-pic').addEventListener('click', function() { characterBuyProcessor(event, characters[1]); });
 
-document.querySelector('.cleveland-pic').addEventListener('click', function() { characterBuyProcessor(event, cleveland); });
+document.querySelector('.cleveland-pic').addEventListener('click', function() { characterBuyProcessor(event, characters[2]); });
 
-document.querySelector('.wp-default-pic').addEventListener('click', function() { backgroundBuyProcessor(event, wp_default) });
+document.querySelector('.wp-default-pic').addEventListener('click', function() { backgroundBuyProcessor(event, wallpapers[0]) });
 
-document.querySelector('.wp-krichev-pic').addEventListener('click', function() { backgroundBuyProcessor(event, wp_krichev) });
+document.querySelector('.wp-krichev-pic').addEventListener('click', function() { backgroundBuyProcessor(event, wallpapers[1]) });
 
-document.querySelector('.wp-tehran-pic').addEventListener('click', function() { backgroundBuyProcessor(event, wp_tehran) });
+document.querySelector('.wp-tehran-pic').addEventListener('click', function() { backgroundBuyProcessor(event, wallpapers[2]) });
